38903177fbc4c7f395cbc8c92335c6133ad38671
nxtxnt <laurentengelbrecht1@gmail.com>
2018-02-13 15:09:15 +0100

    Update sizeof02.asm

--- nasm/sizeof02.asm
+++ nasm/sizeof02.asm
@@ -1,10 +1,30 @@
-  mov eax, [val]
+section .data
 
-  get_numeration:
-    shr eax, 1
-    cmp eax, 0
-    je incr
+section .bss
+  val: dw 1
+  val_as_chars: byte 10
 
-  incr:
-    inc eax
+section .text
+  global _start
+  
+  _start:
+    mov eax, [val]
+    mov ebx, eax
+    call get_numeration
+    
 
+;-----------------------
+;   get the numerator
+
+     get_numeration:
+        shr eax, 1
+        cmp eax, 0
+        je incr
+
+      incr:
+        inc eax
+
+;-----------------------
+;    convert to char
+
+    
