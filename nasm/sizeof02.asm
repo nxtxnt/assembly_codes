369792dce8b2afbb8902c56dd75dd4eb5380db64
nxtxnt <laurentengelbrecht1@gmail.com>
2018-02-13 15:15:14 +0100

    Update sizeof02.asm

--- nasm/sizeof02.asm
+++ nasm/sizeof02.asm
@@ -1,20 +1,30 @@
+; ---------------------------------------------------------------------------------------
+;|second attempt at making a better sizeof that can print variables bigger than one digit|
+; ---------------------------------------------------------------------------------------
+
 section .data
 
 section .bss
-  val: dw 1
-  val_as_chars: byte 10
+  random_data resb 22                   ;data to measure
+  random_data_size resb 4               ;variable that will store the size of random_data
+  random_data_size_as_chars: resb 10    ;variable that will contain the value of random_data_size expressed in chars
 
 section .text
   global _start
   
   _start:
+  
+  
+;-----------------------
+;convert 
+
     mov eax, [val]
     mov ebx, eax
     call get_numeration
     
 
 ;-----------------------
-;   get the numerator
+;   get the numerator   
 
      get_numeration:
         shr eax, 1
