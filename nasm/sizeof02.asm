2bf5bcf717d3558c131d9047ab4201b00b466405
nxtxnt <laurentengelbrecht1@gmail.com>
2018-02-13 21:59:51 +0100

    Update sizeof02.asm

--- nasm/sizeof02.asm
+++ nasm/sizeof02.asm
@@ -8,14 +8,21 @@ section .bss
   random_data resb 22                   ;data to measure
   random_data_size resb 4               ;variable that will store the size of random_data
   random_data_size_as_chars: resb 10    ;variable that will contain the value of random_data_size expressed in chars
+  size_random_data_size_as_chars equ $-random_data_size_as_chars ;we set the array's size at our current
+                                                                 ;location minus the location of the previous variable
 
 section .text
   global _start
   
   _start:
-  
+ 
 ;-----------------------------------------------------------------------------------------
-;                           EXPRESSING OUR RESULT AS CHAR
+;                             GET THE SIZE OF THE RANDOM_DATA
+
+  
+
+;-----------------------------------------------------------------------------------------
+;                           CONVERTING OUR RESULT AS CHAR
 
 ;--------------------------------
 ;         GET NUMERATOR
@@ -23,14 +30,14 @@ section .text
                                   ;units, decimal, hundreds, thousands etc.
 
     init01:
-      mov ax, [random_data_size]  ;we initialize ax with the 
-      mov bl, ax                  ;bl (our counter) is initialized at random_data_size
+      mov al, [random_data_size]  ;we initialize ax with random_data_size let's it's say 123
+      mov bl, al                  ;bl (our counter) is initialized at random_data_size
       
     get_num:
-      div ax, 10                  ;we divide ax by 10 each time, if eax is inferior to 10, bl will end up with null value
-      dec bl                      ;bl is first decremented so always inferior to random_data_size
-      cmp ax, 0
-      call write_on_array
+      mov bh, bl                  ;bh is bl's value kept intact kept intact (123)
+      div bl, 10                  ;we divide ax by 10 each time, if eax is inferior to 10, bl will end up null, (123/10 = 12)
+      cmp bl, 0                   ;<--+ 
+      call write_on_array         ;   |
       jg get_num                  ;while eax is greater than 0
 
 
@@ -41,9 +48,18 @@ section .text
                                   ;(or first if we count from right to left) 1 2 3 4 <--- last digit
                                   ;random_data_size_as_chars's memory address where the digit will be put
         write_on_array:
-          mov cx, ax                ;ax's value will be used to point the location inside
-          div cx, 10                ;123/10 = 12
-          mul cx, 10                ;12*10 = 120
-          mov cx, ax-dx             ;123-120 = 3 we isolated our digit !
-          add cx, 48                ;expressing our digit as a readable character, 48 being the character '0'
-          mov [random_data_size_as_chars+bl], byte cx
+          mov cl, bl                ;ax's value will be used to point the location inside (cx = 12)
+          mov dl, cl                ;dl will be used right after
+          mul cl, 10                ;12*10 = 120
+          mov cl, bh-dl             ;(bh = 123) - (dl = 120) = 3 we isolated our digit !
+          add cl, 48                ;expressing our digit as a readable character, 48 being the character '0'
+          mov [random_data_size_as_chars+bl], byte cl
+
+;-------------------------------------------------------------------------------------------
+;                                   PRINT OUR RESULT
+
+        output:
+          mov eax, 4
+          mov ebx, 1
+          mov ecx, random_data_size_as_chars
+          mov edx, size_random_data_size_as_chars
