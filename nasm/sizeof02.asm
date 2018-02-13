cd1fe4611eb3afb42afe6093c13d08b771c5b97d
nxtxnt <laurentengelbrecht1@gmail.com>
2018-02-13 21:43:53 +0100

    Update sizeof02.asm

--- nasm/sizeof02.asm
+++ nasm/sizeof02.asm
@@ -39,12 +39,11 @@ section .text
                                   ;here we extract the specefic number we want to extract, since ax is divided by 10 each time
                                   ;the number we want to extract next is always the last digit
                                   ;(or first if we count from right to left) 1 2 3 4 <--- last digit
-                                  ;ax's value will be used to point the location inside
                                   ;random_data_size_as_chars's memory address where the digit will be put
         write_on_array:
-          mov dx, ax
-          div dx, 10                ;123/10 = 12
-          mul dx, 10                ;12*10 = 120
-          mov dx, ax-dx             ;123-120 = 3 we isolated our digit !
-          add dx, 48                ;expressing our digit as a readable character, 48 being the character '0'
-          mov [random_data_size_as_chars+bl], byte dx
+          mov cx, ax                ;ax's value will be used to point the location inside
+          div cx, 10                ;123/10 = 12
+          mul cx, 10                ;12*10 = 120
+          mov cx, ax-dx             ;123-120 = 3 we isolated our digit !
+          add cx, 48                ;expressing our digit as a readable character, 48 being the character '0'
+          mov [random_data_size_as_chars+bl], byte cx
